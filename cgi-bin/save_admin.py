59a2bd8ff8646669f4132924aff88e71512afdcb
brunnerj <brunnerj@mit.edu>
2023-10-11 21:31:46 -0400

    .

--- cgi-bin/save_admin.py
+++ cgi-bin/save_admin.py
@@ -7,7 +7,7 @@ cgitb.enable()
 
 print("Content-type: text/plain\n")
 d = cgi.FieldStorage()
-if len(mafia.get_game(d.getfirst("id"))) != int(d.getfirst("count")):
+if len(mafia.get_game(d.getfirst("id")).rstrip()) != int(d.getfirst("count")):
     print('$<p style="color:red">Error: game state has changed, please reload the game</p>{} != {}'.format(len(mafia.get_game(d.getfirst("id"))),int(d.getfirst("count"))))
 else:
     (game, valid, messages) = mafia.save_game(d.getfirst("id").rstrip(),mafia.commands_to_json(d.getfirst("commands").split("\n")))
